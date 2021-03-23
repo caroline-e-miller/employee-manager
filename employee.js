90a1512453e6ad96dffb3c0da86d19d65b550aa8
caroline-e-miller <caroline.miller@ymail.com>
2021-03-23 16:57:44 -0400

    added some inquirer questions and some functions

--- employee.js
+++ employee.js
@@ -15,25 +15,127 @@ const connection = mysql.createConnection({
     database: 'employeeDB',
 });
 
-const queryAllEmployeeData = () => {
-    connection.query('SELECT * FROM employees', (err, res) => {
-        if (err) throw err;
-        res.forEach(({ first_name, last_name, role_id, manager_id, department_name, title, salary, department_id, }) => {
-            console.log(`${first_name} | ${last_name} | ${role_id} | ${manager_id}| ${department_name} | ${title} | ${salary} | ${department_id}`);
+// view all employees / view employees by department / view all employees by manager / add employee / remove employee / update employee role / update employee manager
+
+const startingQuestion = () => {
+    inquirer
+        .prompt({
+            name: 'startQuestion',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['View all employees', 'View employees by department', 'View employees by manager', 'Add employee', 'Remove employee', 'Update employee role', 'Update employee manager'],
+        })
+        .then((answer) => {
+            // based on their answer, either call the bid or the post functions
+            if (answer.startQuestion === 'View all employees') {
+                allEmployees();
+            } else if (answer.startQuestion === 'View employees by department') {
+                departmentEmployees();
+            } else if (answer.startQuestion === 'View employees by manager') {
+                managerEmployees();
+            } else if (answer.startQuestion === 'Add employee') {
+                addEmployee();
+            } else if (answer.startQuestion === 'Remove employee') {
+                removeEmployee();
+            } else if (answer.startQuestion === 'Update employee role') {
+                updateRole();
+            } else if (answer.startQuestion === 'Update employee manager') {
+                updateManager();
+            } else {
+                connection.end();
+            }
         });
-        console.log('-----------------------------------');
-    });
 };
 
-const queryRole = () => {
-    connection.query('SELECT * FROM employees', (err, res) => {
-        if (err) throw err;
-        res.forEach(({ title, salary, department_id }) => {
-            console.log(`${title} | ${salary} | ${department_id} }`);
+const allEmployees = () => {
+    const query = connection.query(
+        'SELECT * FROM employee', (err, res) => {
+            if (err) throw err;
+            res.forEach(({ first_name, last_name, role_id, manager_id }) => {
+                console.log(`${first_name} | ${last_name} | ${role_id} | ${manager_id}`);
+            })
+        }
+    );
+    console.log(query.sql);
+    connection.end();
+
+    inquirer.prompt([
+        {
+            name: 'actionSelect',
+            type: 'list',
+            message: 'What would you like to do with the employee data?',
+            choices: ['View all employees', 'View departments', 'View roles', 'Add employee', 'Add department', 'Add role', 'Update employee role', 'Remove employee'],
+        }
+    ])
+        .then((answer) => {
+            // based on their answer, either call the bid or the post functions
+            if (answer.startQuestion === 'View all employees') {
+                allEmployees();
+            } else if (answer.startQuestion === 'View departments') {
+                departments();
+            } else if (answer.startQuestion === 'View roles') {
+                roles();
+            } else if (answer.startQuestion === 'Add employee') {
+                addEmployee();
+            } else if (answer.startQuestion === 'Add department') {
+                addDepartment();
+            } else if (answer.startQuestion === 'Add role') {
+                addRole();
+            } else if (answer.startQuestion === 'Update employee role') {
+                updateRole();
+            } else if (answer.startQuestion === 'Remove employee') {
+                removeEmployee();
+            } else {
+                connection.end();
+            }
         });
-        console.log('-----------------------------------');
-    });
+}
+
+const departments = () => {
+    const query = connection.query(
+        'SELECT * FROM department', (err, res) => {
+            if (err) throw err;
+            res.forEach(({ department_name }) => {
+                console.log(`${department_name}`);
+            })
+        }
+    );
+    console.log(query.sql);
+    connection.end();
 };
+
+const roles = () => {
+    const query = connection.query(
+        'SELECT * FROM role', (err, res) => {
+            if (err) throw err;
+            res.forEach(({ department_name }) => {
+                console.log(`${department_name}`);
+            })
+        }
+    );
+    console.log(query.sql);
+    connection.end();
+};
+
+// const queryAllEmployeeData = () => {
+//     connection.query('SELECT * FROM employees', (err, res) => {
+//         if (err) throw err;
+//         res.forEach(({ first_name, last_name, role_id, manager_id, department_name, title, salary, department_id, }) => {
+//             console.log(`${first_name} | ${last_name} | ${role_id} | ${manager_id}| ${department_name} | ${title} | ${salary} | ${department_id}`);
+//         });
+//         console.log('-----------------------------------');
+//     });
+// };
+
+// const queryRole = () => {
+//     connection.query('SELECT * FROM employees', (err, res) => {
+//         if (err) throw err;
+//         res.forEach(({ title, salary, department_id }) => {
+//             console.log(`${title} | ${salary} | ${department_id} }`);
+//         });
+//         console.log('-----------------------------------');
+//     });
+// };
 
 
 
@@ -57,6 +159,5 @@ const queryRole = () => {
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`);
-    //   queryAllSongs();
-    //   queryDanceSongs();
+    startingQuestion();
 });
--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -4,36 +4,46 @@ CREATE DATABASE employee_manageDB;
 USE employee_manageDB;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
-),
+);
 
 CREATE TABLE role (
-    id INT NOT NULL,
-    title VARCHAR(30),
-    salary DECIMAL,
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(8,2),
     department_id INT,
-    PRIMARY KEY (id)
-)
+    PRIMARY KEY (id) 
+);
 
 CREATE TABLE employee (
-    id INT NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
     role_id INT,
     manager_id INT NULL,
     PRIMARY KEY (id)
-)
+);
 
 INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Trevor", "King", "Engineer");
+VALUES ("Trevor", "King", 0205);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Bruno", "Buchiarrati", 0503);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Jotaro", "Kujo", 1989);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Manager","500,000.00", "Advertising");
+VALUES ("Manager","200,000.00", 3922);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Vice President","400,000.00", 1187);
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO","500,000.00", 0017);
+
 
 INSERT INTO department (department_name)
 VALUES ("Development");
-
-
-
+INSERT INTO department (department_name)
+VALUES ("Human Resources");
+INSERT INTO department (department_name)
+VALUES ("Administration");
